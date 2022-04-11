c9565cc12f75402121bc47a69a878fbbbb95d3d1
nurmakhan_pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-04-11 14:15:41 +0600

    Add project files.

---
+++ EFCoreDbFromModel/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public ApplicationContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=helloapp.db");
+    }
+}
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
